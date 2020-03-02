28764e8f483872991bfd2015e9733f820c03bec1
Andrew Baik <and.baik@gmail.com>
2020-03-02 01:20:21 -0500

    Changed fn signature of C02E05 for better readability

--- src/main/scala/C02E05.scala
+++ src/main/scala/C02E05.scala
@@ -2,5 +2,5 @@
   * Implement the higher-order function that composes two functions.
   */
 object C02E05 {
-  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
+  def compose[A, B, C](f: A => B, g: B => C): A => C = a => g(f(a))
 }
--- src/test/scala/C02E05Spec.scala
+++ src/test/scala/C02E05Spec.scala
@@ -5,13 +5,15 @@ class C02E05Spec extends FunSpec with Matchers {
     def cube(b: Int): Int = b * b * b
     def addOne(a: Int): Int = a + 1
 
-    it("should compose addOne then cube") {
-      val result = C02E05.compose(cube, addOne)
+    it("should addOne then cube") {
+      val result = C02E05.compose(addOne, cube)
+      // (1 + 1) ^ 3
       result(1) shouldBe 8
     }
 
     it("should cube then addOne") {
-      val result = C02E05.compose(addOne, cube)
+      val result = C02E05.compose(cube, addOne)
+      // 1 ^ 3 + 1
       result(1) shouldBe 2
     }
   }
