c2c3090dec295a7bf88ca2f37dd08404aea50105
Andrew Baik <and.baik@gmail.com>
2020-03-01 23:41:30 -0500

    C02E02 some more unit test cleanup

--- src/test/scala/C02E02Spec.scala
+++ src/test/scala/C02E02Spec.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 class C02E02Spec extends FunSpec with Matchers with BeforeAndAfter {
   describe("isSorted") {
     describe("ordered = (a: Int, b: Int) => a <= b") {
-      var intAsc: (Int, Int) => Boolean = (a: Int, b: Int) => a <= b
+      def intAsc(a: Int, b: Int): Boolean = a <= b
 
       it("as = [1, 2, 3, 4, 5] should be true") {
         C02E02.isSorted((1 to 5).toArray, intAsc) shouldBe true
@@ -15,7 +15,7 @@ class C02E02Spec extends FunSpec with Matchers with BeforeAndAfter {
     }
 
     describe("ordered = (a: Char, b: Char) => a <= b") {
-      val charAsc = (a: Char, b: Char) => a <= b
+      def charAsc(a: Char, b: Char): Boolean = a <= b
 
       it("as = ['a', 'b', 'c', 'd', 'e'] should be true") {
         C02E02.isSorted(('a' to 'e').toArray, charAsc) shouldBe true
