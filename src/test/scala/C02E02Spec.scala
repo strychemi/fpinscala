dd5a4479c0f3925252d0efabb10ad5a56328b147
Andrew Baik <and.baik@gmail.com>
2020-03-01 23:38:38 -0500

    C02E02 unit test refactor

--- src/test/scala/C02E02Spec.scala
+++ src/test/scala/C02E02Spec.scala
@@ -3,24 +3,25 @@ import org.scalatest._
 class C02E02Spec extends FunSpec with Matchers with BeforeAndAfter {
   describe("isSorted") {
     describe("ordered = (a: Int, b: Int) => a <= b") {
+      var intAsc: (Int, Int) => Boolean = (a: Int, b: Int) => a <= b
+
       it("as = [1, 2, 3, 4, 5] should be true") {
-        C02E02.isSorted((1 to 5).toArray, (a: Int, b: Int) => a <= b) shouldBe true
+        C02E02.isSorted((1 to 5).toArray, intAsc) shouldBe true
       }
 
       it("as = [1, 2, 3, 0, 4] should be false") {
-        C02E02.isSorted(Array(1, 2, 3, 0, 4), (a: Int, b: Int) => a <= b) shouldBe false
+        C02E02.isSorted(Array(1, 2, 3, 0, 4), intAsc) shouldBe false
       }
     }
 
     describe("ordered = (a: Char, b: Char) => a <= b") {
+      val charAsc = (a: Char, b: Char) => a <= b
+
       it("as = ['a', 'b', 'c', 'd', 'e'] should be true") {
-        C02E02.isSorted(('a' to 'e').toArray, (a: Char, b: Char) => a <= b) shouldBe true
+        C02E02.isSorted(('a' to 'e').toArray, charAsc) shouldBe true
       }
       it("as = ['a', 'b', 'c', 'a', 'd'] should be false") {
-        C02E02.isSorted(
-          Array('a', 'b', 'c', 'a', 'd'),
-          (a: Char, b: Char) => a <= b
-        ) shouldBe false
+        C02E02.isSorted(Array('a', 'b', 'c', 'a', 'd'), charAsc) shouldBe false
       }
     }
   }
