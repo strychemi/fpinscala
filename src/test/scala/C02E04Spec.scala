c6f4c14433ed65cd50d4f4995ee49e77f3c9a015
Andrew Baik <and.baik@gmail.com>
2020-03-02 00:51:26 -0500

    C02E04 - uncurry

---
+++ src/test/scala/C02E04Spec.scala
@@ -0,0 +1,12 @@
+import org.scalatest._
+
+class C02E04Spec extends FunSpec with Matchers {
+  describe("uncurry") {
+    // test is kind of pointless because of type safety...
+    // having it compile means that it should work as expected
+    it("Int => Double => String should return (Int, Double) => String") {
+      val result = C02E04.uncurry((a: Int) => (b: Double) => s"$a $b")
+      result(1, 0.1) shouldBe "1 0.1"
+    }
+  }
+}
