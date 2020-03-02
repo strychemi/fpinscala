1f5facbb05a185423d62a3282c8cb19738e4abda
Andrew Baik <and.baik@gmail.com>
2020-03-02 01:16:23 -0500

    cleanup tests for C02E05 - compose

--- src/test/scala/C02E05Spec.scala
+++ src/test/scala/C02E05Spec.scala
@@ -1,15 +1,18 @@
 import org.scalatest._
 
 class C02E05Spec extends FunSpec with Matchers {
-  describe("uncurry") {
-    // test is kind of pointless because of type safety...
-    // having it compile means that it should work as expected
-    it("Int => Double => String should return (Int, Double) => String") {
-      def f(b: Int): String = s"$b"
-      def g(a: Int): Int = a + 1
+  describe("compose") {
+    def cube(b: Int): Int = b * b * b
+    def addOne(a: Int): Int = a + 1
 
-      val result = C02E05.compose(f, g)
-      result(1) shouldBe "2"
+    it("should compose addOne then cube") {
+      val result = C02E05.compose(cube, addOne)
+      result(1) shouldBe 8
+    }
+
+    it("should cube then addOne") {
+      val result = C02E05.compose(addOne, cube)
+      result(1) shouldBe 2
     }
   }
 }
