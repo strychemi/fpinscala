58ea9e8ee45fcaad9f1773db0f42ce5dd17ff6e6
Andrew Baik <and.baik@gmail.com>
2020-04-19 00:54:28 -0400

    Touchup on C03E25

--- src/main/scala/Ch03/C03E25.scala
+++ src/main/scala/Ch03/C03E25.scala
@@ -3,7 +3,7 @@ package Ch03
 object C03E25 {
   def size[A](tree: Tree[A]): Int = {
     tree match {
-      case Leaf(value)         => 1
+      case Leaf(_)             => 1
       case Branch(left, right) => 1 + size(left) + size(right)
     }
   }
